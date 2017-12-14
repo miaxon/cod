167c0f484d40fb93dd534ce7d8b4e5c055d3aa8d
conalex <alexcon314@gmail.com>
2017-12-14 06:55:24 +0300

    test gitea origin intra

--- dcim/pages/DCUserPage.cs
+++ dcim/pages/DCUserPage.cs
@@ -26,7 +26,7 @@ namespace dcim.pages
                 Upadate();
             }
         }
-        //edit function edit
+        //edit function edit red
         protected override void Edit()
         {
             List<int> list = view.SelectedIndexes;
