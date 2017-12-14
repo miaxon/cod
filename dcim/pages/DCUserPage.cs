45ee4bfef8a790d012a08bc62085b4ff7d709e20
conalex <alexcon314@gmail.com>
2017-12-14 06:53:47 +0300

    test gitea intra

--- dcim/pages/DCUserPage.cs
+++ dcim/pages/DCUserPage.cs
@@ -26,7 +26,7 @@ namespace dcim.pages
                 Upadate();
             }
         }
-        //edit function edit edit
+        //edit function edit
         protected override void Edit()
         {
             List<int> list = view.SelectedIndexes;
